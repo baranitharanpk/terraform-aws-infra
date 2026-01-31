e567670474f611a1f70975b6ff31af44b31fd193
baranitharanpk <baranitharanpk15@gmail.com>
2026-01-31 07:52:49 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 # outputs.tf
+
 output "vpc_id" {
   description = "ID of the VPC"
   value       = aws_vpc.main.id
@@ -13,10 +14,6 @@ output "security_group_id" {
   description = "ID of the web server security group"
   value       = aws_security_group.web.id
 }
-
-###################
-# Apache Outputs
-###################
 
 output "apache_instance_ids" {
   description = "List of Apache EC2 instance IDs"
@@ -38,10 +35,6 @@ output "apache_urls" {
   value       = formatlist("http://%s", aws_instance.apache[*].public_ip)
 }
 
-###################
-# Nginx Outputs
-###################
-
 output "nginx_instance_ids" {
   description = "List of Nginx EC2 instance IDs"
   value       = aws_instance.nginx[*].id
@@ -62,10 +55,6 @@ output "nginx_urls" {
   value       = formatlist("http://%s", aws_instance.nginx[*].public_ip)
 }
 
-###################
-# Combined Outputs
-###################
-
 output "all_server_ips" {
   description = "All server IP addresses"
   value = {
@@ -82,24 +71,6 @@ output "all_server_urls" {
   )
 }
 
-output "ssh_commands" {
-  description = "SSH commands to connect to all servers"
-  value = concat(
-    formatlist("ssh -i ~/.ssh/webserver-key ubuntu@%s  # Apache-%s", aws_instance.apache[*].public_ip, range(1, var.apache_instance_count + 1)),
-    formatlist("ssh -i ~/.ssh/webserver-key ubuntu@%s  # Nginx-%s", aws_instance.nginx[*].public_ip, range(1, var.nginx_instance_count + 1))
-  )
-}
-
-output "deployment_time" {
-  description = "Timestamp of deployment"
-  value       = timestamp()
-}
-
-output "ansible_inventory_path" {
-  description = "Path to generated Ansible inventory file"
-  value       = local_file.ansible_inventory.filename
-}
-
 output "server_summary" {
   description = "Summary of all servers"
   value = {
@@ -109,12 +80,8 @@ output "server_summary" {
   }
 }
 
-###################
-# Inventory Content Output for Jenkins
-###################
-
+# NEW OUTPUT - Required for Jenkins pipeline
 output "ansible_inventory_content" {
-  description = "Ansible inventory content for Jenkins to copy"
+  description = "Ansible inventory file content"
   value       = local_file.ansible_inventory.content
-  sensitive   = false
 }
