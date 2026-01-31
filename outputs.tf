2162eea50e7d8db64a762833144baa1c72833ea4
baranitharanpk <baranitharanpk15@gmail.com>
2026-01-31 11:26:15 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-# outputs.tf
 
 output "vpc_id" {
   description = "ID of the VPC"
@@ -15,20 +14,18 @@ output "security_group_id" {
   value       = aws_security_group.web.id
 }
 
-# ---------------- APACHE ----------------
-
 output "apache_instance_ids" {
   description = "List of Apache EC2 instance IDs"
   value       = aws_instance.apache[*].id
 }
 
 output "apache_public_ips" {
-  description = "List of Apache server public IP addresses"
+  description = "List of Apache public IP addresses"
   value       = aws_instance.apache[*].public_ip
 }
 
 output "apache_private_ips" {
-  description = "List of Apache server private IP addresses"
+  description = "List of Apache private IP addresses"
   value       = aws_instance.apache[*].private_ip
 }
 
@@ -37,20 +34,18 @@ output "apache_urls" {
   value       = formatlist("http://%s", aws_instance.apache[*].public_ip)
 }
 
-# ---------------- NGINX ----------------
-
 output "nginx_instance_ids" {
   description = "List of Nginx EC2 instance IDs"
   value       = aws_instance.nginx[*].id
 }
 
 output "nginx_public_ips" {
-  description = "List of Nginx server public IP addresses"
+  description = "List of Nginx public IP addresses"
   value       = aws_instance.nginx[*].public_ip
 }
 
 output "nginx_private_ips" {
-  description = "List of Nginx server private IP addresses"
+  description = "List of Nginx private IP addresses"
   value       = aws_instance.nginx[*].private_ip
 }
 
@@ -59,10 +54,8 @@ output "nginx_urls" {
   value       = formatlist("http://%s", aws_instance.nginx[*].public_ip)
 }
 
-# ---------------- COMBINED ----------------
-
 output "all_server_ips" {
-  description = "All server IP addresses"
+  description = "All server public IP addresses"
   value = {
     apache = aws_instance.apache[*].public_ip
     nginx  = aws_instance.nginx[*].public_ip
@@ -78,17 +71,10 @@ output "all_server_urls" {
 }
 
 output "server_summary" {
-  description = "Summary of all servers"
+  description = "Summary of deployed servers"
   value = {
     total_apache_servers = var.apache_instance_count
     total_nginx_servers  = var.nginx_instance_count
     total_servers        = var.apache_instance_count + var.nginx_instance_count
   }
-}
-
-# -------- REQUIRED FOR JENKINS / DEBUG --------
-
-output "ansible_inventory_content" {
-  description = "Rendered Ansible inventory file content"
-  value       = local_file.ansible_inventory.content
 }
